ccc34bebd732540e3761156e2c87cf0a9ce50f6e
K K <jtroi-dev@yandex.ru>
2026-04-15 10:40:52 +0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Test Script ProductStar
+
+Домашнее задание написание Bash-скрипт.
+
+## 🚀 Быстрый запуск
+
+```bash
+chmod +x tests.sh
+./tests.sh
---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+declare -i a 
+declare -i b
+declare -i c
+
+a=28
+b=4
+c=$a+$b
+
+echo $c
+
+c="Good"
+
+echo $c
+
+d=64;
+e=1
+f=$d+$e
+
+echo $f
